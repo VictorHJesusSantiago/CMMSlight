3cb789e8f278558afef10ef40fa88a0394700476
victor santiago <victorhenriquedejesussantiago@gmail.com>
2026-07-24 09:39:38 -0300

    feat: Add NotFoundException class for handling not found errors in the API

---
+++ cmms-api/src/main/java/com/cmmslight/cmmsapi/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.cmmslight.cmmsapi.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
