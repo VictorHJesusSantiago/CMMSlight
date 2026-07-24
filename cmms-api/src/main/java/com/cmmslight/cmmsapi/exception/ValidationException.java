ceaa84e72581174bcd3a237603fc204bf4e9d83b
victor santiago <victorhenriquedejesussantiago@gmail.com>
2026-07-24 09:39:46 -0300

    feat: Add ValidationException class for handling validation errors in the API

---
+++ cmms-api/src/main/java/com/cmmslight/cmmsapi/exception/ValidationException.java
@@ -0,0 +1,7 @@
+package com.cmmslight.cmmsapi.exception;
+
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
