63f7ee51a782cb616cf2ab80ca49347f3214bfc5
victor santiago <victorhenriquedejesussantiago@gmail.com>
2026-07-25 07:59:09 -0300

    feat: Add unit test for application context loading

---
+++ cmms-api/src/test/java/com/cmmslight/cmmsapi/CmmsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmmslight.cmmsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmmsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
